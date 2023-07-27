ba56badc0c9ebc59bd2a63311abb704edce1571d
Hitoshi Amano <seijro@gmail.com>
2023-07-28 05:06:15 +0900

    rough impl for download

---
+++ src/main.rs
@@ -0,0 +1,216 @@
+use gofile_api::{
+    Api,
+    Content,
+    ContentKind,
+};
+
+use clap::{
+    Parser,
+    Subcommand,
+};
+
+use url::{
+    Url,
+};
+
+use uuid::{
+    Uuid,
+};
+
+use std::{
+    error,
+    env::{
+        self,
+        VarError,
+    },
+    fmt::{
+        self,
+        Display,
+        Formatter,
+    },
+};
+
+use futures::{
+    StreamExt,
+};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Download {
+        #[arg(value_parser = ContentId::parse_content_id)]
+        content_id: ContentId,
+    },
+}
+
+#[derive(Debug)]
+enum Error {
+    TokenNotPresent,
+    TokenNotUnicode,
+    InvalidUrl(Url),
+    InvalidContentUrl(Url),
+    InvalidDownloadUrl(Url),
+    InvalidTopLevelFile(String),
+    NoContent,
+    NotImplementedForSubdir,
+    HttpRequestError(reqwest::Error),
+    GoFileApiError(gofile_api::Error),
+    FileCouldntBeCreated(String),
+    FileCouldntBeWritten(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "()")
+    }
+}
+
+impl error::Error for Error {
+}
+
+impl From<VarError> for Error {
+    fn from(err: VarError) -> Self {
+        match err {
+            VarError::NotPresent => Self::TokenNotPresent,
+            VarError::NotUnicode(_) => Self::TokenNotUnicode,
+        }
+    }
+}
+
+impl From<gofile_api::Error> for Error {
+    fn from(err: gofile_api::Error) -> Self {
+        Self::GoFileApiError(err)
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Self::HttpRequestError(err)
+    }
+}
+
+#[derive(Clone, Debug)]
+enum ContentId {
+    DownloadUrl(Url, String),
+    Uuid(Uuid),
+    Code(String),
+}
+
+impl ContentId {
+    fn parse_content_id(content_id_str: &str) -> Result<ContentId, Error> {
+        if let Ok(uuid) = Uuid::parse_str(content_id_str) {
+            return Ok(ContentId::Uuid(uuid));
+        };
+        if let Ok(url) = Url::parse(content_id_str) {
+            let Some(mut segs) = url.path_segments() else {
+                return Err(Error::InvalidUrl(url));
+            };
+            match segs.next() {
+                Some("d") => {
+                    let Some(code) = segs.next() else {
+                        return Err(Error::InvalidContentUrl(url));
+                    };
+
+                    if let Some(_) = segs.next() {
+                        return Err(Error::InvalidContentUrl(url));
+                    };
+
+                    return Ok(ContentId::Code(String::from(code)))
+                },
+                Some("download") => {
+                    let Some(uuid_str) = segs.next() else {
+                        return Err(Error::InvalidDownloadUrl(url));
+                    };
+                    let Ok(_) = Uuid::parse_str(uuid_str) else {
+                        return Err(Error::InvalidDownloadUrl(url));
+                    };
+                    let Some(filename) = segs.next() else {
+                        return Err(Error::InvalidDownloadUrl(url));
+                    };
+
+                    if let Some(_) = segs.next() {
+                        return Err(Error::InvalidDownloadUrl(url));
+                    };
+
+                    return Ok(ContentId::DownloadUrl(url.clone(), String::from(filename)));
+                    
+                },
+                _ => return Err(Error::InvalidUrl(url)),
+            };
+        };
+        Ok(ContentId::Code(String::from(content_id_str)))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let args = Cli::parse();
+    match args.command {
+        Command::Download { content_id } => {
+            download(content_id).await
+        },
+    }
+}
+
+async fn download(content_id: ContentId) -> Result<(), Error> {
+    let api = Api::new();
+    let api = api.authorize(get_token()?);
+    match content_id {
+        ContentId::DownloadUrl(url, filename) => download_impl(url, filename).await,
+        ContentId::Uuid(id) => {
+            let content = api.get_content_by_id(id).await?;
+            download_all_child_contents(content).await
+        },
+        ContentId::Code(code) => {
+            let content = api.get_content_by_code(code).await?;
+            download_all_child_contents(content).await
+        },
+    }
+}
+
+async fn download_impl(url: Url, filename: String) -> Result<(), Error> {
+    let res = reqwest::get(url).await?;
+    let mut byte_stream = res.bytes_stream();
+    let mut file = match tokio::fs::File::create(filename).await {
+        Ok(file) => file,
+        Err(err) => return Err(Error::FileCouldntBeCreated(format!("{}", err))),
+    };
+    while let Some(item) = byte_stream.next().await {
+        match tokio::io::copy(&mut item?.as_ref(), &mut file).await {
+            Err(err) => return Err(Error::FileCouldntBeWritten(format!("{}", err))),
+            Ok(_) => (),
+        }
+    }
+    Ok(())
+}
+
+async fn download_all_child_contents(content: Content) -> Result<(), Error> {
+    let ContentKind::Folder { contents, .. } = content.kind else {
+        return Err(Error::InvalidTopLevelFile(content.name));
+    };
+    let Some(contents) = contents else {
+        return Err(Error::NoContent);
+    };
+    if 0 == contents.len() {
+        return Err(Error::NoContent);
+    };
+
+    for (_, content) in contents {
+        let ContentKind::File { link, .. } = content.kind else {
+            return Err(Error::NotImplementedForSubdir);
+        };
+        download_impl(link, content.name).await?
+    };
+    Ok(())
+}
+
+fn get_token() -> Result<String, Error> {
+    Ok(env::var("GOFILE_TOKEN")?)
+}
+
